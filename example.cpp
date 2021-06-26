16d1fea2d5223e601d0a395bbeefa5821e8c763f
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2021-06-26 01:27:50 -0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,24 +1,18 @@
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-// Custom character binarys (Put your code in the brackets)
-const char hearth[8] PROGMEM = {0x00, 0x0A, 0x1F, 0x1F, 0x0E, 0x04, 0x00, 0x00};
+// Custom character binaries (Put your code in the brackets)
+byte hearth[8] = {0x00, 0x0A, 0x1F, 0x1F, 0x0E, 0x04, 0x00, 0x00};
 
-void setup()
-{
-    lcd.init();
-
+void setup() {
     // Custom character registration
     lcd.createChar(0, hearth);
-}
 
-void loop()
-{
-    lcd.setCursor(0, 0);
-
+    lcd.begin(16, 2);
+    
     // Draws the custom character
-    lcd.write(0);
-
-    delay(500);
+    lcd.write(byte(0));
 }
+
+void loop() {}
