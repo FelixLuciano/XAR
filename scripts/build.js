613e2cdcf8f0c8c9be719aa259088f98fd51ae02
FelixLuciano <felix.lucianodias@gmail.com>
2021-06-28 01:28:22 -0300

    Folder structure & CSS bundling

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,15 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.12.10/mod.js"
 
 await esbuild.build({
-    entryPoints: ["./js/main.js"],
+    entryPoints: ["./src/js/main.js"],
     outfile: "./app.js",
+    target: "esnext",
+    bundle: true
+})
+
+await esbuild.build({
+    entryPoints: ["./src/css/style.css"],
+    outfile: "./style.css",
     bundle: true,
     minify: true
 })
